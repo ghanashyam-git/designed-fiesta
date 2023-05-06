765b98734464b254990f2cf5ca8f2df366104fb6
ghanashyam-git <132386719+ghanashyam-git@users.noreply.github.com>
2023-05-06 18:06:10 +0000

    doubtful

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # print(__name__) -to print where it is invoked from-->main
 
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 @app.route("/") #To apply which path to match
 
 def hello_world():
-  return "Helloo Ghana"
+  return render_template('home.html')
 
 if(__name__ == '__main__'):
   app.run(host='0.0.0.0',debug=True)
